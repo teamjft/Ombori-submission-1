1e98b51fe84d3729c4324e14b4007c6381b4d7bd
Amit <amit@amittechguy.in>
2018-06-19 21:12:43 +0530

    Implemented required feature

---
+++ app/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import SplashScreen from '../pages/splashscreen/splashscreen';
+
+const App = () => (
+  <SplashScreen />
+);
+
+export default App;
---
+++ utility/networking.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const serverUrl = 'https://reqres.in';
+
+const apiFetchUser = `${serverUrl}/api/users?page=`;
+
+export default async function fetchUserList(params) {
+  try {
+    const response = await axios.get(apiFetchUser + params);
+    console.warn(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+  return {};
+}
