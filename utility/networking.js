8518c65b8e27791f00656e8bb8190cd867ffaaa6
Amit <amit@amittechguy.in>
2018-06-19 21:21:29 +0530

    removed console statement

--- utility/networking.js
+++ utility/networking.js
@@ -7,10 +7,8 @@ const apiFetchUser = `${serverUrl}/api/users?page=`;
 export default async function fetchUserList(params) {
   try {
     const response = await axios.get(apiFetchUser + params);
-    console.warn(response.data);
     return response.data;
   } catch (error) {
-    console.error(error);
+    return { error };
   }
-  return {};
 }
